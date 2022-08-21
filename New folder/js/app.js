462ac684629b298494f08730fed68510eccab98e
nahian255 <nahimn253@gmail.com>
2022-08-22 01:23:25 +0600

    html done and start js

--- New folder/js/app.js
+++ New folder/js/app.js
@@ -1,1 +1,18 @@
-console.log('come on')
+const cardArray = [];
+
+function display(cardPlayer) {
+    console.log(cardPlayer);
+}
+
+function addToCard(element) {
+
+    // console.log(element.parentNode.parentNode.children[0].innerText);
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+
+    const playerObj = {
+        playerName: playerName,
+    }
+    cardArray.push(playerObj);
+
+    display(cardArray);
+}
